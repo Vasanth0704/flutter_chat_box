e2ca9827f4d99a73dac708b2a6ce2a6d40ba9cfa
vasanth <153159819+Vasanth0704@users.noreply.github.com>
2025-01-20 15:36:31 +0530

    chat ui added

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:bubble/bubble.dart';
 
 class HomeScreen extends StatefulWidget {
-
   final String title;
 
   const HomeScreen({super.key, required this.title});
@@ -14,11 +14,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(widget.title),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Bubble(
+              margin: const BubbleEdges.only(top: 10),
+              alignment: Alignment.topLeft,
+              nip: BubbleNip.leftTop,
+              color: Colors.blueAccent,
+              child: Text(
+                'Hello, this is a message inside a bubble!',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 10),
+            Bubble(
+              margin: const BubbleEdges.only(top: 10),
+              alignment: Alignment.topRight,
+              nip: BubbleNip.rightTop,
+              color: Colors.green,
+              child: Text(
+                'This is another bubble message!',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
