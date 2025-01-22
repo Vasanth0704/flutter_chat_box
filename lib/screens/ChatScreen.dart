497e9576dcca464ff5d2355cc16c179f52db0bbc
Vasanth0704 <a20csdc36@sjctnc.edu.in>
2025-01-22 14:17:14 +0530

    Create ChatScreen.dart

---
+++ lib/screens/ChatScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+
+  final String title;
+
+  const ChatScreen({super.key, required this.title});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
