69fe4926b32918c89ab9fde135efad2b81158761
Vasanth0704 <a20csdc36@sjctnc.edu.in>
2025-01-22 14:44:07 +0530

    Home Screen Ui added

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bubble/bubble.dart';
+import 'package:flutter_chat_box/screens/CallScreen.dart';
+import 'package:flutter_chat_box/screens/ChatScreen.dart';
+import 'package:flutter_chat_box/screens/StatusScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   final String title;
@@ -11,41 +14,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int _currentIndex = 0;
+
+  final List<Widget> _screens =[
+    ChatScreen(title: 'Chat',),
+    StatusScreen(title: 'Status',),
+    CallScreen(title: 'Call',)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
 
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Bubble(
-              margin: const BubbleEdges.only(top: 10),
-              alignment: Alignment.topLeft,
-              nip: BubbleNip.leftTop,
-              color: Colors.blueAccent,
-              child: Text(
-                'Hello, this is a message inside a bubble!',
-                style: TextStyle(color: Colors.white),
-              ),
+          onTap: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chat',
             ),
-            SizedBox(height: 10),
-            Bubble(
-              margin: const BubbleEdges.only(top: 10),
-              alignment: Alignment.topRight,
-              nip: BubbleNip.rightTop,
-              color: Colors.green,
-              child: Text(
-                'This is another bubble message!',
-                style: TextStyle(color: Colors.white),
-              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.circle),
+              label: 'Status',
             ),
-          ],
-        ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Call',
+            ),
+          ]
       ),
     );
   }
