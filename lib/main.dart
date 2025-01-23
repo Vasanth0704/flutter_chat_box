3d248b56e66939fc107ef180975b9bd610808c60
Vasanth0704 <a20csdc36@sjctnc.edu.in>
2025-01-23 15:14:03 +0530

    Welcome Screen added to main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_box/screens/HomeScreen.dart';
+import 'package:flutter_chat_box/screens/WelcomeScreen.dart';
 
 void main()
 {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(title: 'Home',),
+      home: WelcomeScreen(),
     );
   }
 }
