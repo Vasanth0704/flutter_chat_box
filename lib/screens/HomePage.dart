5d6ce3116c2dd5bb0313ba8d976b891e953968d8
varshini250707 <153159819+Vasanth0704@users.noreply.github.com>
2025-02-25 14:19:21 +0530

    Update HomePage.dart

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,58 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:bubble/bubble.dart';
 import 'package:flutter_chat_box/screens/CallScreen.dart';
 import 'package:flutter_chat_box/screens/ChatScreen.dart';
 import 'package:flutter_chat_box/screens/StatusScreen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class HomeScreen extends StatefulWidget {
+final SupabaseClient _supabase = Supabase.instance.client;
+
+class HomePage extends StatefulWidget {
   final String title;
 
-  const HomeScreen({super.key, required this.title});
+  const HomePage({super.key, required this.title});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-
+class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
-
-  final List<Widget> _screens =[
-    ChatScreen(title: 'Chat',),
-    StatusScreen(title: 'Status',),
-    CallScreen(title: 'Call',)
-  ];
 
   @override
   Widget build(BuildContext context) {
+    final user = _supabase.auth.currentUser;
+    final String userName = user?.userMetadata?['name'] ?? 'User';
+
+    final List<Widget> _screens = [
+      ChatScreen(title: 'Chat $userName'),
+      StatusScreen(title: 'Status'),
+      CallScreen(title: 'Call'),
+    ];
+
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
       body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-
-          onTap: (index){
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              label: 'Chat',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.circle),
-              label: 'Status',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.call),
-              label: 'Call',
-            ),
-          ]
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.circle),
+            label: 'Status',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call),
+            label: 'Call',
+          ),
+        ],
       ),
     );
   }
