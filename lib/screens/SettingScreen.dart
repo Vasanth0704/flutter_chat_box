5713b313b3bf68e4f529b7f4c0e804a9160e5af5
VASANTHARAJAN <a20csdc36@sjctnc.edu.in>
2025-03-16 14:59:43 +0530

    Update SettingScreen.dart

--- lib/screens/SettingScreen.dart
+++ lib/screens/SettingScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_box/screens/ChatsScreen.dart';
 import 'package:flutter_chat_box/screens/HomePage.dart';
-import 'package:flutter_chat_box/screens/ProfileScreen.dart';
+import 'package:flutter_chat_box/screens/auth/ProfileScreen.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'auth/LoginScreen.dart';
